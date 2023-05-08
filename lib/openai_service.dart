49e77bae8df3dbe8f83567c38102a50f8dedb69a
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-05-08 15:41:37 +0100

    added the open ai configuration in the openAi service.dart file

--- lib/openai_service.dart
+++ lib/openai_service.dart
@@ -1,1 +1,44 @@
-class OpenAIService {}
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'secret.dart';
+
+class OpenAIService {
+  Future<String> isArtPromptAPI(String prompt) async {
+    try {
+      final res = await http.post(
+        Uri.parse('https://api.openai.com/v1/chat/completions'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $openAIAPIKey'
+        },
+        body: jsonEncode({
+          "model": "gpt-3.5-turbo",
+          "messages": [
+            {
+              'role': 'user',
+              'content':
+                  'Does this message want to generate an AI picture, image, art or anything similar? $prompt . Simply answer with a yes or no.',
+            }
+          ],
+        }),
+      );
+      print(res.body);
+      if (res.statusCode == 200) {
+        print('yay');
+      }
+      return 'AI';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  Future<String> chatGPTAPI(String prompt) async {
+    return 'CHATGPT';
+  }
+
+  Future<String> dallEAPI(String prompt) async {
+    return 'DALL-E';
+  }
+}
